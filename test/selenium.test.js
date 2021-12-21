9f2a1bb88e032f6ad9e36a514f0a5498c0955dfd
Aleksandra Jasoovic <aleksandra.jasovic.git@gmail.com>
2021-12-21 09:04:07 +0100

    New Math functions update

--- test/selenium.test.js
+++ test/selenium.test.js
@@ -16,5 +16,11 @@ describe('Maths test', async function() {
     it('Test if 4 - 3 = 1', async function() {
         assert.equal(MyMath.sub(4,3), 1);
     });
+    it('Test if 16 / 4 = 4', async function() {
+        assert.equal(MyMath.divide(16,4), 4);
+    });
+    it('Test if 2 * 3 * 5 = 30', function() {
+        assert.equal(MyMath.multiply(2, 3, 5), 30);
+    });
 
 });
