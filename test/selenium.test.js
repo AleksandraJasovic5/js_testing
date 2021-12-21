7e21f6a6f590ad7a808070dbc67f114c29df0c74
Aleksandra Jasoovic <aleksandra.jasovic.git@gmail.com>
2021-12-21 09:27:43 +0100

    Added unit.test.js

--- test/selenium.test.js
+++ test/selenium.test.js
@@ -2,25 +2,12 @@
 
 const webdriver = require('selenium-webdriver');
 const assert = require('assert');
-const MyMath = require('../Maths');
+
 
 describe('Selenium Tests', function() {
     it('Test if 1 equals 1', async function() {
         assert.equal(1,1);
     });
 });
-describe('Maths test', async function() {
-    it('Test if 1 + 1 = 2', function() {
-        assert.equal(MyMath.add(1,1), 2);
-    });
-    it('Test if 4 - 3 = 1', async function() {
-        assert.equal(MyMath.sub(4,3), 1);
-    });
-    it('Test if 16 / 4 = 4', async function() {
-        assert.equal(MyMath.divide(16,4), 4);
-    });
-    it('Test if 2 * 3 * 5 = 30', function() {
-        assert.equal(MyMath.multiply(2, 3, 5), 30);
-    });
 
-});
+
